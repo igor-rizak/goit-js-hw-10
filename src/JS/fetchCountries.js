47b6ad0e5f213740623b0f207decf4ddc2446f58
igor-rizak <igor.rizak@ukr.net>
2023-05-23 11:33:39 +0300

    Update fetchCountries.js

--- src/JS/fetchCountries.js
+++ src/JS/fetchCountries.js
@@ -1,8 +1,8 @@
 const URL =  `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
 
 export function fetchCountries(name) {
-    return fetch(URL
-    ).then(response => {
+  return fetch(URL)
+    .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
